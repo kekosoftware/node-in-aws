327ff70bc4a18d5470f70e1ff932cb2d4849b75b
Gustavo Ariel Coronel <ariel256@gmail.com>
2021-10-25 10:40:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const helmet = require('helmet');
+var compression = require('compression');
+require('dotenv').config();
+const {v4: uuidv4} = require('uuid');
+
+const app = express();
+app.use(helmet());        // Ayuda a proteger aplicaciones Express
+app.use(compression());
+
+// Servidor HTTP
+const serverHttp = http.createServer(app);
+serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
+
+app.use(express.static('./public'));
+
+// Test
+app.get('/api/get-uuid', function(req, res) {
+    res.send(uuidv4())
+});
+
+
+app.get('*', function(req, res) {
+  res.status(404).send("Error 404 - Recurso no disponible")
+});
