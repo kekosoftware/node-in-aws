c22fa2a5b616f8d0a5c5755c284f35e686389c0f
Gustavo Ariel Coronel <ariel256@gmail.com>
2021-10-31 07:46:01 -0300

    Add https

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
 const helmet = require('helmet');
 var compression = require('compression');
 require('dotenv').config();
 const {v4: uuidv4} = require('uuid');
+const cors = require('cors');
+
+const httpsServerOptions = {
+  key: fs.readFileSync(process.env.KEY_PATH),
+  cert: fs.readFileSync(process.env.CERT_PATH),
+}
+
 
 const app = express();
 app.use(helmet());        // Ayuda a proteger aplicaciones Express
 app.use(compression());
+app.use(cors());
 
 // Servidor HTTP
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
 
+const serverHttps = https.createServer(httpsServerOptions, app);
+serverHttps.listen(process.env.HTTPS_PORT, process.env.IP);
+
+// Redirect
+app.use((req, res, next) => {
+  if(req.secure) next(); else res.redirect(`https://${req.headers.host}${req.url}`);
+});
+
+// Static content
 app.use(express.static('./public'));
 
 // Test
